48c903cf98d39c10912737669283b08f6bd6bf90
liudexiao <2567206637@qq.com>
2026-05-23 12:09:25 +0800

    优化aarch64配置

--- build/CMakeFiles/3.22.1/CMakeSystem.cmake
+++ build/CMakeFiles/3.22.1/CMakeSystem.cmake
@@ -3,13 +3,13 @@ set(CMAKE_HOST_SYSTEM_NAME "Linux")
 set(CMAKE_HOST_SYSTEM_VERSION "6.6.114.1-microsoft-standard-WSL2")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
+include("/home/liu/gsdk/GSDK/cmake/toolchain-aarch64.cmake")
 
-
-set(CMAKE_SYSTEM "Linux-6.6.114.1-microsoft-standard-WSL2")
+set(CMAKE_SYSTEM "Linux")
 set(CMAKE_SYSTEM_NAME "Linux")
-set(CMAKE_SYSTEM_VERSION "6.6.114.1-microsoft-standard-WSL2")
-set(CMAKE_SYSTEM_PROCESSOR "x86_64")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "aarch64")
 
-set(CMAKE_CROSSCOMPILING "FALSE")
+set(CMAKE_CROSSCOMPILING "TRUE")
 
 set(CMAKE_SYSTEM_LOADED 1)
